08e4fcee6cae04f62d87fde737302409f8bea9d2
Bryan Chan <xchangip@gmail.com>
2018-01-13 19:59:57 -0600

    Moving squares

--- src/js/app.js
+++ src/js/app.js
@@ -17,13 +17,14 @@ class App extends React.Component {
   }
 
   _onMouseMove(e) {
-    this.setState({ x: e.screenX, y: e.screenY })
+    var bounds = document.getElementById("playground").getBoundingClientRect();
+    this.setState({ x: e.clientX - bounds.left, y: e.clientY - bounds.top })
     emitPositionChange({ position: { x: this.state.x, y: this.state.y } })
   }
 
   render() {
     return (
-      <div className="App" onMouseMove={ this._onMouseMove.bind(this) } >
+      <div className="App">
         <h1>Mouse coordinates: { this.state.x } { this.state.y }</h1>
         <ul>
           {
@@ -36,12 +37,26 @@ class App extends React.Component {
                     <ul>
                     <li> X: { client.position.x } </li>
                     <li> Y: { client.position.y } </li>
+                    <li> Color: { client.color } </li>
                     </ul>
                   </div> 
                 </li>
               )}) : null
           }
         </ul>
+
+        <div 
+          id="playground" 
+          className="playground" 
+          style={{position: "absolute", height:350, width:900, animationDuration: "0.3s", border:"solid #000000"}}
+          onMouseMove={ this._onMouseMove.bind(this) }>
+          {this.state.clients.length ? this.state.clients.map( (client, index) => { 
+            return (
+              <div key={index} style={{position: "relative", top:client.position.y, left:client.position.x, backgroundColor:client.color, height:10, width:10}}>
+              </div>
+            )
+          }) : null}
+        </div>
       </div>
     )
   }
