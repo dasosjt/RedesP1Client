8512b221976b3f4f3100ddca2bfc363f4bb48f0e
Luisa2125 <luiyi2125@gmail.com>
2018-03-07 01:51:58 -0600

    genera comida aleatoria

--- src/js/app.js
+++ src/js/app.js
@@ -9,20 +9,23 @@ class App extends React.Component {
     this.state = {
       clients: [],
       x: 0,
-      y: 0
+      y: 0,
+      comida: []
     }
+
   }
 
   componentDidMount(){
-    subscribeToState((err, state) => { this.setState({ clients: state.clients }) })
+    subscribeToState((err, state) => { this.setState({ clients: state.clients, comida:state.comida }) })
+  
+
   }
 
   _onMouseMove(e) {
-    if(e.target.id == "playground") {
-      var bounds = document.getElementById("playground").getBoundingClientRect();
-      this.setState({ x: e.clientX - bounds.left, y: e.clientY - bounds.top })
-      emitPositionChange({ position: { x: this.state.x, y: this.state.y } })
-    }
+    var bounds = document.getElementById("playground").getBoundingClientRect();
+    this.setState({ x: e.clientX - bounds.left, y: e.clientY - bounds.top })
+    emitPositionChange({ position: { x: this.state.x, y: this.state.y } })
+    emitColision(0.01)
   }
 
   render() {
@@ -37,17 +40,31 @@ class App extends React.Component {
             id="playground" 
             className="app-game__playground"
             onMouseMove={ this._onMouseMove.bind(this) }>
-            {this.state.clients.length ? this.state.clients.map( (client, index) => { 
+            <div>
+             <div className="app-game__players-list__player-cards">
+            {this.state.clients.length ? this.state.clients.map( (client,index) => { 
+              //console.log(this.state.comida[0][index])
               return (
                 <div key={index} style={{position: "absolute", top:client.position.y, left:client.position.x, backgroundColor:client.color, height:(client.score < 10) ? 10 : client.score, width:(client.score < 10) ? 10 : client.score}}>
                 </div>
               )
             }) : null}
+            </div>
+            {this.state.comida.length ? this.state.comida.map( (comida,index) => { 
+              //console.log(this.state.comida[0][index].x,this.state.comida[0][index].y)
+          
+              return (
+                  <div key={index} style={{position: "absolute", top:comida.y, left:comida.x, backgroundColor:comida.color, height: 10 , width:10}}>
+                  </div>
+              )
+                            
+              
+            }) : null}
+          </div> 
           </div>
 
           <div className="app-game__players-list">
             <h3 className="app-game__players-list__title">Jugadores</h3>
-            <div className="app-game__players-list__player-cards">
               {
                 this.state.clients.length
                 ? this.state.clients.map( (client, index) => { 
@@ -61,7 +78,6 @@ class App extends React.Component {
                         score={ client.score } />
                   )}) : null
               }
-            </div>
           </div>
         </div>
       </div>
